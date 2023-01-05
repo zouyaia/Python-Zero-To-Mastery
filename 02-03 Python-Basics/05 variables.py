35d00060d486d950651562833c3f8dbfb48bd0c4
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-06 00:15:00 +0300

    learned variable types

---
+++ 02-03 Python-Basics/05 variables.py
@@ -0,0 +1,15 @@
+# variables should have intuitive names <>_<>
+_user_Iq = 110
+
+float = 190
+print(float)
+
+print(_user_Iq)
+
+# constants - capital letters
+PI = 3.14
+
+# __name__ is a special variables, don't touch these ones __xxx__
+a,b,c = 1,2,3
+print(a,b,c)
+# Naming vars is one of the most important skills in prog!!!
