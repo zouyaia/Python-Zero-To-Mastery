1cd958e3381f77ab855f8ff12ea3dcf79a9e3257
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-06 00:18:09 +0300

    py strings init

---
+++ 02-03 Python-Basics/08 strings.py
@@ -0,0 +1,19 @@
+a = type("hi hellot 24")
+print(type(a))
+username = 'supercoder'
+password = 'supresecret'
+
+long_string = '''
+WOW
+O O
+---
+'''
+print(long_string)
+
+first_name = 'George'
+last_name = 'Washington'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+# string concatenation 
+print('hello' + ' Andrei')
