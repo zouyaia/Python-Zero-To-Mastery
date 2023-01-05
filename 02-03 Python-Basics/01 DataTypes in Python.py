ddd8c0a76103fd329773ff0039a34686c69670ab
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-05 23:51:27 +0300

    main datatypes

---
+++ 02-03 Python-Basics/01 DataTypes in Python.py
@@ -0,0 +1,18 @@
+#Standard Data Types [lesson 018 on hard_drive]
+int
+float
+# complex
+str
+bool
+set
+list
+dict
+tuple
+
+#Classes - custom modifiable Data Types
+SuperData
+
+#Specialized DataTypes
+DataTypes from python Modules
+
+None
---
+++ 02-03 Python-Basics/02 int and float.py
@@ -0,0 +1,21 @@
+# Fundamental Data Types
+# int and float 
+# print(type(3+2))
+# print(type(3-4))
+# print(type(3/4))
+# print(type(4*3))
+
+# print(type(9.9+1.1))
+# # power
+# print(2 ** 3)
+# # round down to an int
+# print(2 // 4)
+# # modulo - remainder of division 
+# print(5 % 4)
+# # float problem
+# print(0.1 + 0.2 == 0.3, 0.1 + 0.2)
+# print()
+
+# math functions
+print(round(3.9))
+print(abs(-29))
