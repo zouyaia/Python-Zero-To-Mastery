cd359523b81c1e01fe5cc08d4906f5215e669d84
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-07 01:20:16 +0300

    learned to format string type

---
+++ 02-03 Python-Basics/11 Formatted Strings.py
@@ -0,0 +1,9 @@
+# formatted strings
+
+name = 'Johnny'
+items = 13
+
+# new version fast&easy way
+print(f'hi {name}. You have {items} products in the cart')
+# old-school way (has many variations on using)
+print('hi {1}. You have {0} products in the cart'.format('rob', 11))
