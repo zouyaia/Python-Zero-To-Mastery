dbe9c01b947403c96db09c1ed9a5bb5a6edb8afc
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-08 11:39:34 +0300

    string methods

---
+++ 02-03 Python-Basics/14 string Func+Methods.py
@@ -0,0 +1,10 @@
+# Built-in Functions + Methods
+# greet = 'hellloooo'
+# print(greet[0:len(greet)])
+
+quote = 'to be or not to be'
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
