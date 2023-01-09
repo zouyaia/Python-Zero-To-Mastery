114989c95958a173bcd50d5ef81047457acabc3b
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-09 13:29:24 +0300

    lists that are two dimensional

---
+++ 02-03 Python-Basics/19 Matrix.py
@@ -0,0 +1,51 @@
+# Matrix
+matrix = [
+  [1,0,1],
+  [0,9,0],
+  [1,0,1]
+]
+
+print(matrix[1][1])
+
+# Exercise
+# using this list: 
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+# access "Oranges" and print it:
+# You will find the answer if you scroll down to the bottom, but attempt it yourself first!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution:
+# basket[1][1][0]
