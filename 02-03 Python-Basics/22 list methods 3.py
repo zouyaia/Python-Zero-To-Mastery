431d9af072a4977f3047cf1357917516fbfe62c5
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-10 09:51:38 +0300

    list methods

---
+++ 02-03 Python-Basics/22 list methods 3.py
@@ -0,0 +1,15 @@
+basket = ['a','x','b','c','a']
+# basket.sort()
+
+print(sorted(basket))
+print(basket)
+
+basket.reverse()
+print(basket)
+
+# common list patterns
+print(list(range(1,100)))
+# make a string from a list
+new_sentence = ' '.join(['hi', 'my', 'name', 'is', 'Joe'])
+print(new_sentence)
+
