087fd979d2c46babe5ab66f56094b472c57697c0
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-10 09:53:24 +0300

    keys of a dictionary

---
+++ 02-03 Python-Basics/25 Dictionary Keys.py
@@ -0,0 +1,10 @@
+dictionary = {
+  '123': [1,2,3],
+  'b': 'hello',
+  True: 24,
+  '123': 'twice'
+}
+# dictionary key must be immutable, no list is allowed
+
+print(dictionary[True])
+print(dictionary[123])
