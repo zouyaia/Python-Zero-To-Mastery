0db494d93115c1a291499f57d47f74498fb71f85
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-15 11:42:29 +0300

    OOP definition

---
+++ 06 Advanced OOP/115 what is oop.py
@@ -0,0 +1,9 @@
+# OOP: organizing our code
+class BigObject:  # class
+    pass
+
+
+obj1 = BigObject()  # instanciate
+obj2 = BigObject()  # instanciate
+obj3 = BigObject()  # instanciate
+print(type(obj1))
