fe4d6a118b4fc3724021c7f08eaa1f050b2d502e
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-21 09:50:38 +0300

    list comprehensions

---
+++ 07 Advanced Functional Programming/147 Comprehensions.py
@@ -0,0 +1,8 @@
+# list, set, dictionary
+my_list = [char for char in 'hello']
+my_list2 = [num*2 for num in range(0, 100)]
+my_list3 = [num**2 for num in range(1, 100) if num%2==0] # list conditions
+print(my_list3)
+# for char in 'hello':
+#     my_list.append(char)
+# print(my_list)
