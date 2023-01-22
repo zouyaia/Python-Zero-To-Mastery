1d2d3ee6dbd55d7b6a7a7f84250769af83cdbd57
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-22 08:56:38 +0300

    python decorators part 3

---
+++ 08 Advanced Python  Decorators/154 Decorators3.py
@@ -0,0 +1,13 @@
+# Decorator Pattern is used all over programming
+def my_decorator(func):
+    def wrap_func(*args, **kwargs):
+        print('*****')
+        func(*args, **kwargs)
+        print('*****')
+    return wrap_func
+
+@my_decorator
+def hello(greeting, emoji=':('):
+    print(greeting, emoji)
+    
+hello('hiiii')
