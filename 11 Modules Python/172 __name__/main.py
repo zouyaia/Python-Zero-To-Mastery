b14d107b31af0c2fc78bb1bf380b7d80fee0a750
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-27 10:37:40 +0300

    __name__ python namespace

---
+++ 11 Modules Python/172 __name__/main.py
@@ -0,0 +1,5 @@
+from shopping.more_shopping import shopping_cart
+from utility import mult, div
+
+if __name__ == '__main__': # if this is the file being run
+    print("main.py being run")
---
+++ 11 Modules Python/172 __name__/utility.py
@@ -0,0 +1,9 @@
+print(__name__)
+def mult(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1/num2
+
+if __name__ == '__main__':
+    print("utility.py being run")
