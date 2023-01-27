a44e2118baf98017f95a9aa9f7fdaf4af4e23b3b
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-27 10:37:12 +0300

    ways to import modules

---
+++ 11 Modules Python/171 Ways to import Modules/main.py
@@ -0,0 +1,5 @@
+from shopping.more_shopping import shopping_cart
+from utility import mult, div
+
+print(div(8, 3))
+print(shopping_cart.buy("t-shirt"))
---
+++ 11 Modules Python/171 Ways to import Modules/utility.py
@@ -0,0 +1,5 @@
+def mult(num1, num2):
+    return num1*num2
+
+def div(num1, num2):
+    return num1/num2
