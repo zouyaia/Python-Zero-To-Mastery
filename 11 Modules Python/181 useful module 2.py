cb59b3ccda0522698ddd1be4a528906dc758ef1a
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-01-29 11:38:39 +0300

    useful modules part 2

---
+++ 11 Modules Python/181 useful module 2.py
@@ -0,0 +1,8 @@
+import datetime
+from time import time
+from array import array
+
+print(datetime.date.today())
+print(time())
+arr = array('i', [4, 7, 9]) # quicker bc of specific types stored initially 'i' = int
+print(arr[1])
