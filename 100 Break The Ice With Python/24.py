fa2cddcd8d41f97bdce4daf2673d481121e858df
zouyaia <122051534+zouyaia@users.noreply.github.com>
2023-02-05 17:33:23 +0300

    problem number 24

---
+++ 100 Break The Ice With Python/24.py
@@ -0,0 +1,11 @@
+def square(x):
+    """Return the square of a number (x^2)"""
+    return x**2
+
+print(abs.__doc__)
+print()
+print(int.__doc__)
+print()
+print(input.__doc__)
+print()
+print(square.__doc__)
